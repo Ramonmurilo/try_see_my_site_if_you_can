08bffd034d315e744c51692645f9e2dada665b02
Ramonmurilo <ramonmurilo@id.uff.br>
2026-05-16 21:49:36 +0000

    :sparkles: feat(landing): first page done, landing page

--- src/landing-page.tsx
+++ src/landing-page.tsx
@@ -1,10 +1,60 @@
 import "react"
+import "./landing-page.css"
+import { useState } from "react";
 
 function LandingPage() {
+
+
+    function StartButton() {
+
+        const [count, setCount] = useState(0);
+        const stopped = count >=10;
+
+        const handleMouseOver = () => {
+            if (stopped) return;
+
+            const btn = document.getElementById("start-button") as HTMLButtonElement;
+            const maxX = window.innerWidth - btn.offsetWidth;
+            const maxY = window.innerHeight - btn.offsetHeight;
+            btn.style.left = `${Math.floor(Math.random()*maxX)}px`;
+            btn.style.top = `${Math.floor(Math.random()* maxY)}px`;
+            btn.style.transform = 'none';
+
+            setCount(c => c + 1);
+            
+        }
+        const centeredStyle = { left: '50%', top: '50%', transform: 'translate(-50%, -50%)' };
+
+        return (
+            <button 
+            id="start-button" 
+            onMouseMove={handleMouseOver}
+            style={stopped ? centeredStyle : {}}
+            >
+                {stopped ? 'its over! new features soon!' : "Let's GOOO"}
+            </button>
+        );
+    }
+
     return (
         <>
             <section id="first-message">
-                first message! Hello!
+                
+                <div className="texto-inicial">
+
+                    <h1> Hello, welcome to my site! Enjoy!</h1>
+                    
+                    <div className="texto-siga">
+                        let's start!
+                    </div>
+
+                    <div className="buttons">
+                        <StartButton/>
+                    </div>
+                </div>
+
+
+
             </section>
         </>
     )
